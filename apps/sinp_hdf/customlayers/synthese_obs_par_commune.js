565db92a06e25d62b945d763646b3807b88d8c64
mducrocq <m.ducrocq@cbnbl.org>
2025-08-18 13:48:08 +0200

    First configuration init

---
+++ apps/sinp_hdf/customlayers/synthese_obs_par_commune.js
@@ -0,0 +1,10 @@
+let geojson_layer = new ol.format.GeoJSON();
+
+const layer =  new ol.layer.Vector({
+    source: new ol.source.Vector({
+        url: 'http://localhost:8080/geoserver/sinp_test_dev/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=sinp_test_dev%3Amat_synthese_obs_par_commune_full_tooltip&outputFormat=application%2Fjson',
+        format: geojson_layer
+    }),
+
+});
+new CustomLayer('obs_par_commune', layer);
