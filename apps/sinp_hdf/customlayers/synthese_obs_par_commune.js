0e23ab6c2068814d583311c611337f348f8632d6
mducrocq <m.ducrocq@cbnbl.org>
2025-08-22 07:56:36 +0200

    maj cfg

--- apps/sinp_hdf/customlayers/synthese_obs_par_commune.js
+++ apps/sinp_hdf/customlayers/synthese_obs_par_commune.js
@@ -1,10 +1,10 @@
 let geojson_layer = new ol.format.GeoJSON();
 
-const layer =  new ol.layer.Vector({
-    source: new ol.source.Vector({
-        url: 'http://localhost:8080/geoserver/sinp_test_dev/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=sinp_test_dev%3Amat_synthese_obs_par_commune_full_tooltip&outputFormat=application%2Fjson',
-        format: geojson_layer
-    }),
-
+const layer = new ol.layer.Vector({
+  source: new ol.source.Vector({
+    url: "http://localhost:8080/geoserver/sinp_hdf_dev/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=sinp_test_dev%3Av_synthese_evt_par_date_et_commune&outputFormat=application%2Fjson",
+    format: geojson_layer,
+  }),
 });
-new CustomLayer('mat_synthese_obs_par_commune_full_tooltip', layer);
+/*TODO Filtrage dynamique*/
+new CustomLayer("synthese_evt_par_date_et_commune", layer);
